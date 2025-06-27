8d024c5ea2fef9c15b380afc72961e70b99d1652
Yashitha-Gillella <gillellayashitha@gmail.com>
2025-06-27 15:34:26 +0530

    Fetching a character from string using negative index

--- indexing.py
+++ indexing.py
@@ -1,4 +1,4 @@
-a=yashitha gillella
+a="yashitha gillella"
 print("given string is: ",a)
-print("We are fetching g in the given string using index")
+print("We are fetching a in the given string using index")
 print(a[9])
---
+++ string_indexing_ng.py
@@ -0,0 +1,4 @@
+a="yashitha gillella"
+print("The string :",a)
+print("Fetching character g from string using negative indexing")
+print(a[-8])
