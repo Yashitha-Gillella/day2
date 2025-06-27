a7bdc97bc665e0ac9513cd98b5e978224a33e38d
Yashitha-Gillella <gillellayashitha@gmail.com>
2025-06-27 22:35:21 +0530

    Fetching characters from string using indexing

---
+++ task4_stringindexing.py
@@ -0,0 +1,5 @@
+name="Binladin"
+print("Fetching characters of word Binladin")
+print("We are fetching midlle letters L from back and A from frontwards:",name[-5],name[4])
+print("We are fetching second I from backward:",name[-7])
+print("We are fetching first I from backward:",name[-2])
