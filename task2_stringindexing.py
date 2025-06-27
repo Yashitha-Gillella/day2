adbe4148cdf209d2e7e705a486d9a4e856ca72c1
Yashitha-Gillella <gillellayashitha@gmail.com>
2025-06-27 22:30:24 +0530

    updaying changes

--- task2_stringindexing.py
+++ task2_stringindexing.py
@@ -1,4 +1,5 @@
 name="Allu Arjun"
+print("Name is:",name)
 print("We are fetching second A from backward:",name[-5])
 print("We are fetching J from backward:",name[-3])
 print("We are fetching second L from frontward:",name[2])
