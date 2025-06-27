1e3fc7986c6d49c7802405b45d1257e3456cab37
Yashitha-Gillella <gillellayashitha@gmail.com>
2025-06-27 22:33:39 +0530

    fetching characters from string using front and backward indexing

---
+++ task3_stringindexing.py
@@ -0,0 +1,7 @@
+name="Sony Bravia"
+print("Name is:",name)
+print("We are fetching B from backward:",name[-6])
+print("We are fetching space from frontward:",name[4])
+print("We are fetching I from frontward:",name[9])
+print("We are fetching last A from frontward:",name[10])
+print("We are fetching S from backwards:",name[-11])
