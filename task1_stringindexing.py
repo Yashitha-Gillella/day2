8ddfbef96607128b698dab096bf0bcc50744f0da
Yashitha-Gillella <gillellayashitha@gmail.com>
2025-06-27 21:58:12 +0530

    fetching characters using backward and frontward indexing

---
+++ task1_stringindexing.py
@@ -0,0 +1,6 @@
+name='harry potter'
+print("name is: ",name)
+print("we are fetching space from backwards:",name[-7])
+print("We are fetching first occurance of t in frontwards:",name[8])
+print("We are fetching second occurance of r in backwards:",name[-9])
+print("We are fetching o from backwards:",name[-5])
